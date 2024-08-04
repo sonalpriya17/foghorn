fd18a31d84d75363e010305e8d8a7122e448bc43
sonalpriya17 <sonalpriya172@gmail.com>
2024-08-05 00:03:58 +0530

    compile.sh executable

--- compile.sh
+++ compile.sh
@@ -4,3 +4,4 @@ bun build --compile --target=bun-linux-arm64-v1.1.4 --outfile ./target/foghorn-l
 bun build --compile --target=bun-windows-x64-v1.1.4 --outfile ./target/foghorn-windows-x64 ./index.js
 bun build --compile --target=bun-mac-arm64-v1.1.4 --outfile ./target/foghorn-mac-arm64 ./index.js
 bun build --compile --target=bun-mac-x64-v1.1.4 --outfile ./target/foghorn-mac-x64 ./index.js
+chmod 755 ./target/foghorn-*
